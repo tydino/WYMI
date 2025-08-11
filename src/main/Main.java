abd0fd7ed94af4e950fba7a1a4db1c6889666181
tydinocreations <tydino909@gmail.com>
2025-08-10 22:31:29 -0400

    a moving box
    not much to go off of yet!

---
+++ src/main/Main.java
@@ -0,0 +1,27 @@
+package main;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("WYMI");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack(); //causes window to be sized to fit the perferred size and layouts of its subcomponents
+
+        window.setLocationRelativeTo(null);//center when null
+        window.setVisible(true);
+
+        gamePanel.startGameThread();
+    }
+}
+
+
+//being made off of basis of this series https://youtu.be/om59cwR7psI
