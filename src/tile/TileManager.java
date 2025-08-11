44bf19f250512996119385f4acd55deab88d3e5b
tydinocreations <tydino909@gmail.com>
2025-08-11 00:29:34 -0400

    additional tiles
    should be noted that i do want to seperate the trees to their own tilemap but that wont be for a while!

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -19,7 +19,7 @@ public class TileManager {
     public TileManager(GamePanel gp){
         this.gp = gp;
 
-        tile = new Tile[4];
+        tile = new Tile[7];
         mapTileNum = new int[gp.maxScreenCol][gp.maxScreenRow];
 
         getTileImage();
@@ -29,17 +29,33 @@ public class TileManager {
     public void getTileImage(){
         try{
 
+            //grass
             tile[0] = new Tile();
             tile[0].image = ImageIO.read(getClass().getClassLoader().getResourceAsStream("tiles/grass.png"));
 
+            //wall
             tile[1] = new Tile();
             tile[1].image = ImageIO.read(getClass().getClassLoader().getResourceAsStream("tiles/wall.png"));
 
+            //water
             tile[2] = new Tile();
             tile[2].image = ImageIO.read(getClass().getClassLoader().getResourceAsStream("tiles/water.png"));
 
+            //dirt path
             tile[3] = new Tile();
             tile[3].image = ImageIO.read(getClass().getClassLoader().getResourceAsStream("tiles/dirt_path.png"));
+
+            //dirt
+            tile[4] = new Tile();
+            tile[4].image = ImageIO.read(getClass().getClassLoader().getResourceAsStream("tiles/dirt.png"));
+
+            //sand
+            tile[5] = new Tile();
+            tile[5].image = ImageIO.read(getClass().getClassLoader().getResourceAsStream("tiles/sand.png"));
+
+            //small tree on grass
+            tile[6] = new Tile();
+            tile[6].image = ImageIO.read(getClass().getClassLoader().getResourceAsStream("tiles/smalltree_grass.png"));
 
         }catch(IOException e){
             e.printStackTrace();
