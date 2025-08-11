3748f9d559e91cb002fe59a418cbfc618843f443
tydinocreations <tydino909@gmail.com>
2025-08-10 23:29:43 -0400

    moving arrow in circle
    yep not much else added, though i do know now that animations are very simmilar to that of minecraft! (only i have to do frame swap outs manually but how the work on a basis level is very simmilar!)

---
+++ src/entities/Player.java
@@ -0,0 +1,119 @@
+package entities;
+
+import main.GamePanel;
+import main.KeyHandler;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.nio.Buffer;
+
+public class Player extends Entity{
+
+    GamePanel gp;
+    KeyHandler keyH;
+
+    public Player(GamePanel gp, KeyHandler keyH){
+        
+        this.gp = gp;
+        this.keyH = keyH;
+
+        setDefaultValues();
+        getPlayerImage();
+    }
+    public void setDefaultValues(){
+        x=100;
+        y=100;
+        speed=4;
+        direction = "down";
+    }
+
+    public void getPlayerImage(){
+        try{
+            up1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/test_up1.png"));
+            up2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/test_up2.png"));
+            down1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/test_down1.png"));
+            down2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/test_down2.png"));
+            left1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/test_left1.png"));
+            left2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/test_left2.png"));
+            right1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/test_right1.png"));
+            right2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/test_right2.png"));
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void update() {
+
+        if(keyH.upPressed || keyH.downPressed || keyH.leftPressed || keyH.rightPressed) {
+
+            if (keyH.upPressed) {
+                direction = "up";
+                y -= speed;
+            } else if (keyH.downPressed) {
+                direction = "down";
+                y += speed;
+            } else if (keyH.leftPressed) {
+                direction = "left";
+                x -= speed;
+            } else if (keyH.rightPressed) {
+                direction = "right";
+                x += speed;
+            }
+
+            spriteCounter++;
+            if (spriteCounter > 15) {
+                if (spriteNum == 1) {
+                    spriteNum = 2;
+                } else if (spriteNum == 2) {
+                    spriteNum = 1;
+                }
+                spriteCounter = 0;
+            }
+        }
+
+    }
+    public void draw(Graphics2D g2){
+
+        BufferedImage image = null;
+
+        switch(direction){
+            case "up":
+                if(spriteNum == 1) {
+                    image = up1;
+                }
+                if(spriteNum == 2){
+                    image = up2;
+                }
+                break;
+            case "down":
+                if(spriteNum == 1) {
+                    image = down1;
+                }
+                if(spriteNum == 2){
+                    image = down2;
+                }
+                break;
+            case "left":
+                if(spriteNum == 1) {
+                    image = left1;
+                }
+                if(spriteNum == 2){
+                    image = left2;
+                }
+                break;
+            case "right":
+                if(spriteNum == 1) {
+                    image = right1;
+                }
+                if(spriteNum == 2){
+                    image = right2;
+                }
+                break;
+        }
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+
+    }
+}
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,5 +1,7 @@
 package main;
 
+import entities.Player;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -9,7 +11,7 @@ public class GamePanel extends JPanel implements Runnable{
     final int originalTileSize = 16;///oTS x XoTStile size
     final int scale = 3;//s*oTS
 
-    final int tileSize = originalTileSize * scale; //48x48 tiles will be displayed
+    public final int tileSize = originalTileSize * scale; //48x48 tiles will be displayed
     final int maxScreenCol = 16; // horizonatl/left right
     final int maxScreenRow = 12; // vertical/up down
     final int screenWidth = tileSize * maxScreenCol;
@@ -20,6 +22,7 @@ public class GamePanel extends JPanel implements Runnable{
 
     KeyHandler  keyH = new KeyHandler();
     Thread gameThread;
+    Player player = new Player(this, keyH);
 
     //set player's default position
     int playerX = 100;
@@ -69,16 +72,7 @@ public class GamePanel extends JPanel implements Runnable{
     }
     public void update(){
 
-        if (keyH.upPressed) {
-            playerY -= playerSpeed;
-        }
-        else if (keyH.downPressed) {
-            playerY += playerSpeed;
-        }else if (keyH.leftPressed) {
-            playerX -= playerSpeed;
-        }else if (keyH.rightPressed) {
-            playerX += playerSpeed;
-        }
+        player.update();
 
     }
     public void paintComponent(Graphics g){
@@ -87,9 +81,7 @@ public class GamePanel extends JPanel implements Runnable{
 
         Graphics2D g2 = (Graphics2D)g;
 
-        g2.setColor(Color.white);
-
-        g2.fillRect(playerX, playerY, tileSize, tileSize);
+        player.draw(g2);
 
         g2.dispose();
     }
