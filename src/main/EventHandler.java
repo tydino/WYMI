3a08c524af065b457213dd8d24dd8de651c5831f
tydinocreations <tydino909@gmail.com>
2025-08-12 12:40:32 -0400

    adjusted event system + tripping
    now after teleporting or going to the place you get teleported to there is an event where you "trip" and take damage!
    
    Signed-off-by: tydinocreations <tydino909@gmail.com>

--- src/main/EventHandler.java
+++ src/main/EventHandler.java
@@ -1,40 +1,51 @@
 package main;
 
-import java.awt.*;
-
 public class EventHandler {
 
     GamePanel gp;
-    Rectangle eventRect;
-    int eventRectDefaultX, eventRectDefaultY;
+    EventRect[][] eventRect;
 
     public EventHandler(GamePanel gp){
         this.gp = gp;
 
-        eventRect = new Rectangle();
-        eventRect.x = 12;
-        eventRect.y = 12;
-        eventRect.width = 24;
-        eventRect.height = 24;
-        eventRectDefaultX = eventRect.x;
-        eventRectDefaultY = eventRect.y;
+        eventRect = new EventRect[gp.maxWorldCol][gp.maxWorldRow];
+
+        int col = 0;
+        int row = 0;
+        while(col<gp.maxWorldCol && row < gp.maxWorldRow){
+
+            eventRect[col][row] = new EventRect();
+            eventRect[col][row].x = 12;
+            eventRect[col][row].y = 12;
+            eventRect[col][row].width = 24;
+            eventRect[col][row].height = 24;
+            eventRect[col][row].eventRectDefaultX = eventRect[col][row].x;
+            eventRect[col][row].eventRectDefaultY = eventRect[col][row].y;
+
+            col++;
+            if(col == gp.maxWorldCol){
+                col=0;
+                row++;
+            }
+        }
     }
 
     public void checkEvent(){
         //minus 1 to both x, and y for the column and rows. (starts at 0x0 and not 1x1)
-        if (hit(12, 12, "any")){teleport(46, 11, gp.dialogueState);}
-        if(hit(10, 24, "up")){fullhealing(gp.dialogueState);}
+        if (hit(12, 12, "any")){teleport(12, 12, 46, 11, gp.dialogueState, false);}
+        if (hit(44, 15, "any")){damage(44, 15, gp.dialogueState, true);}
+        if (hit(10, 24, "up")){fullhealing(10, 24, gp.dialogueState, false);}
     }
 
-    public boolean hit(int eventcol, int eventRow, String reqDirection){
+    public boolean hit(int col, int row, String reqDirection){
         boolean hit = false;
 
         gp.player.solidArea.x = gp.player.WorldX + gp.player.solidArea.x;
         gp.player.solidArea.y = gp.player.WorldY + gp.player.solidArea.y;
-        eventRect.x = eventcol*gp.tileSize + eventRect.x;
-        eventRect.y = eventRow*gp.tileSize + eventRect.y;
+        eventRect[col][row].x = col*gp.tileSize + eventRect[col][row].x;
+        eventRect[col][row].y = row*gp.tileSize + eventRect[col][row].y;
 
-        if(gp.player.solidArea.intersects(eventRect)){
+        if(gp.player.solidArea.intersects(eventRect[col][row]) && !eventRect[col][row].eventDone){
             if(gp.player.direction.contentEquals(reqDirection) || reqDirection.contentEquals("any")) {
                 hit = true;
             }
@@ -42,32 +53,35 @@ public class EventHandler {
 
         gp.player.solidArea.x = gp.player.solidAreaDefaultX;
         gp.player.solidArea.y = gp.player.solidAreaDefaultY;
-        eventRect.x = eventRectDefaultX;
-        eventRect.y = eventRectDefaultY;
+        eventRect[col][row].x = eventRect[col][row].eventRectDefaultX;
+        eventRect[col][row].y = eventRect[col][row].eventRectDefaultY;
 
         return hit;
     }
 
-    public void damage(int gameState){
+    public void damage(int col, int row, int gameState, boolean oneTime){
 
         gp.gameState = gameState;
         gp.ui.currentDialogue = "Miss fortune befalls you. \n It took some health!";
         gp.player.life -=1;
+        eventRect[col][row].eventDone = oneTime;
     }
 
-    public void teleport(int x, int y, int gameState){
+    public void teleport(int col, int row, int x, int y, int gameState, boolean oneTime){
         gp.gameState = gameState;
         gp.ui.currentDialogue = "You've been teleported!";
         gp.player.WorldX = gp.tileSize * x;
         gp.player.WorldY = gp.tileSize * y;
+        eventRect[col][row].eventDone = oneTime;
     }
 
-    public void fullhealing(int gameState){
+    public void fullhealing(int col, int row, int gameState, boolean oneTime){
 
         if(gp.keyH.enterPressed){
             gp.gameState = gameState;
             gp.ui.currentDialogue = "You gain health.";
             gp.player.life = gp.player.maxLife;
+            eventRect[col][row].eventDone = oneTime;
         }
     }
 
