740438596b41112f10bc860b80e2fada0ea9d825
tydinocreations <tydino909@gmail.com>
2025-08-15 00:16:33 -0400

    added corrupt appleton
    only 2 will spawn for now!
    
    beware they are a level 3+ enemy unlike the fluffles!
    (until level 2 with the axe in hand you cannot even damage them!)
    
    Signed-off-by: tydinocreations <tydino909@gmail.com>

--- src/main/AssetSetter.java
+++ src/main/AssetSetter.java
@@ -1,7 +1,9 @@
 package main;
 
 import entities.NPC_Clickles;
+import monster.MON_CorruptAppleton;
 import monster.MON_Fluffle;
+import objects.OBJ_Axe_grassBladed;
 import objects.OBJ_BasicKey;
 import objects.OBJ_DoubleDoor;
 import objects.OBJ_NumNum_Vial;
@@ -28,6 +30,10 @@ public class AssetSetter {
         gp.obj[i] = new OBJ_NumNum_Vial(gp, 4);
         gp.obj[i].WorldX = gp.tileSize * 35;
         gp.obj[i].WorldY = gp.tileSize * 17;
+        i++;
+        gp.obj[i] = new OBJ_Axe_grassBladed(gp);
+        gp.obj[i].WorldX = gp.tileSize * 15;
+        gp.obj[i].WorldY = gp.tileSize * 16;
         i++;
     }
 
@@ -70,6 +76,14 @@ public class AssetSetter {
         gp.monster[i] = new MON_Fluffle(gp);
         gp.monster[i].WorldX = gp.tileSize * 27;
         gp.monster[i].WorldY = gp.tileSize * 11;
+        i++;
+        gp.monster[i] = new MON_CorruptAppleton(gp);
+        gp.monster[i].WorldX = gp.tileSize * 22;
+        gp.monster[i].WorldY = gp.tileSize * 19;
+        i++;
+        gp.monster[i] = new MON_CorruptAppleton(gp);
+        gp.monster[i].WorldX = gp.tileSize * 22;
+        gp.monster[i].WorldY = gp.tileSize * 21;
         i++;
     }
 }
---
+++ src/monster/MON_CorruptAppleton.java
@@ -0,0 +1,79 @@
+package monster;
+
+import entities.Entity;
+import main.GamePanel;
+
+import java.util.Random;
+
+public class MON_CorruptAppleton extends Entity {
+
+    public MON_CorruptAppleton(GamePanel gp){
+        super(gp);
+
+        type = type_monster;
+        name = "Corrupt Appleton";
+        speed = 3;
+        maxLife = 20;
+        life = maxLife;
+        attack = 6;
+        defense = 2;
+        exp = 4;
+
+        solidArea.x = gp.scale*3;
+        solidArea.y = gp.scale*10;
+        solidArea.width = gp.scale*8;
+        solidArea.height = gp.scale*5;
+        solidAreaDefaultX = solidArea.x;
+        solidAreaDefaultY = solidArea.y;
+
+        getImage();
+    }
+
+    public void getImage() {
+        up1 = setup("npc", "corrupt/appleton", "appleton6", 1, 1);
+        up2 = setup("npc", "corrupt/appleton", "appleton7", 1, 1);
+        down1 = setup("npc", "corrupt/appleton", "appleton0", 1, 1);
+        down2 = setup("npc", "corrupt/appleton", "appleton1", 1, 1);
+        left1 = setup("npc", "corrupt/appleton", "appleton4", 1, 1);
+        left2 = setup("npc", "corrupt/appleton", "appleton5", 1, 1);
+        right1 = setup("npc", "corrupt/appleton", "appleton2", 1, 1);
+        right2 = setup("npc", "corrupt/appleton", "appleton3", 1, 1);
+    }
+
+    public void setAction(){
+
+        actionLockCount++;
+
+        if(actionLockCount == 40) {//every 2 seconds
+            Random random = new Random();
+            int i = random.nextInt(100) + 1;//picks from 0-99 so +1 fixxes it
+
+            if (i <= 25) {
+                direction = "up";
+            }
+            if (i > 25 && i <= 50) {
+                direction = "down";
+            }
+            if (i > 50 && i <= 75) {
+                direction = "left";
+            }
+            if (i > 75 && i <= 100) {
+                direction = "right";
+            }
+
+            actionLockCount = 0;
+        }
+    }
+
+    public void damageReaction(){
+
+        actionLockCount = 0;
+        switch (gp.player.direction) {
+            case "up": direction = "down"; break;
+            case "down": direction = "up"; break;
+            case "left": direction = "right"; break;
+            case "right": direction = "left"; break;
+
+        }
+    }
+}
